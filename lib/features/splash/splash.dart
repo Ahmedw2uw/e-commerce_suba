faff7f801ae802314e9cb69bb75c03384903eb23
Ahmed_Elgendy <136897512+AhmedElgendy2004@users.noreply.github.com>
2025-11-03 22:40:20 +0200

    add animation in splash screen with lottie package

--- lib/features/splash/splash.dart
+++ lib/features/splash/splash.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:e_commerce/core/theme/app_colors.dart';
-import 'package:e_commerce/core/utilits/app_assets.dart';
 import 'package:e_commerce/features/auth/login/login.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String routeName = '/'; // route name for splash
@@ -17,19 +17,18 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 2), () {
+    Timer(const Duration(seconds: 6), () {
       Navigator.pushReplacementNamed(context, Login.routeName);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold( 
+    return Scaffold(
       backgroundColor: AppColors.blue,
       body: Center(
         child: SizedBox(
-          height: 150,
-          child: Image(image: AssetImage(AppImages.logo)),
+          child: Lottie.asset("assets/lottie/add to cart (1).json"),
         ),
       ),
     );
